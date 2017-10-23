dbfa70f9e7a807d076dd35cc20fb2b42098f0adb
Hazurl <ricoujules69@gmail.com>
2017-10-23 17:07:45 +0200

    entity_mask

--- include/ecs/policy/mask_policy.hpp
+++ include/ecs/policy/mask_policy.hpp
@@ -6,64 +6,20 @@ ECS_BEGIN_NS
 
 struct tag_mask_policy {};
 
-struct entity_16_48_mask {
+template<ui8 id_bits_size, ui8 version_bits_size>
+struct entity_mask {
     using tag = tag_mask_policy;
-
-    static constexpr i64 total_size     = 8;
-
-    static constexpr i64 id_size        = 6;
-    static constexpr i64 version_size   = 2;
     
-    static constexpr i64 id_shift       = 0;
-    static constexpr i64 version_shift  = 48;
-
-    static constexpr i64 id_mask        = 0x0000FFFFFFFFFFFF;
-    static constexpr i64 version_mask   = 0x000000000000FFFF;
-};
-
-struct entity_8_24_mask {
-    using tag = tag_mask_policy;
-
-    static constexpr i64 total_size     = 4;
-
-    static constexpr i64 id_size        = 3;
-    static constexpr i64 version_size   = 1;
+    static constexpr i64 total_size     = (id_bits_size + version_bits_size) / 8 + bool((id_bits_size + version_bits_size) % 8);
     
-    static constexpr i64 id_shift       = 0;
-    static constexpr i64 version_shift  = 24;
-
-    static constexpr i64 id_mask        = 0x00FFFFFF;
-    static constexpr i64 version_mask   = 0x000000FF;
-};
-
-struct entity_4_12_mask {
-    using tag = tag_mask_policy;
-
-    static constexpr i64 total_size     = 2;
-    
-    static constexpr i64 id_size        = 2;
-    static constexpr i64 version_size   = 1;
+    static constexpr i64 id_size        = (id_bits_size      ) / 8 + bool((id_bits_size     ) % 8);
+    static constexpr i64 version_size   = (version_bits_size ) / 8 + bool((version_bits_size) % 8);
 
     static constexpr i64 id_shift       = 0;
-    static constexpr i64 version_shift  = 12;
+    static constexpr i64 version_shift  = id_bits_size;
 
-    static constexpr i64 id_mask        = 0x0FFF;
-    static constexpr i64 version_mask   = 0x000F;
-};
-
-struct entity_2_6_mask {
-    using tag = tag_mask_policy;
-
-    static constexpr i64 total_size     = 1;
-    
-    static constexpr i64 id_size        = 1;
-    static constexpr i64 version_size   = 1;
-
-    static constexpr i64 id_shift       = 0;
-    static constexpr i64 version_shift  = 6;
-
-    static constexpr i64 id_mask        = 0b00111111;
-    static constexpr i64 version_mask   = 0b00000011;
+    static constexpr i64 id_mask        = ~((~0LL) << id_bits_size);
+    static constexpr i64 version_mask   = ~((~0LL) << version_bits_size);
 };
 
 ECS_END_NS
--- src/main_ecs.cpp
+++ src/main_ecs.cpp
@@ -29,20 +29,5 @@ namespace std {
 
 int main (int , char** ) {
     using namespace ECS_NS_ECS;
-
-    EntityManager<entity_4_12_mask> em;
-
-    auto e0 = em.create();
-    auto e1 = em.create();
-
-    std::cout << e0 << std::endl << e1 << std::endl;
-    std::cout << em.alive(e0) << std::endl;
-    em.destroy(e0);
-    std::cout << em.alive(e0) << std::endl;
-    auto e2 = em.create();
-    std::cout << e2 << std::endl;
-    em.destroy(e0);
-    em.destroy(e0);    
-    
     return 0;
 }
