611d38f1477d9c22c6a75f848b146ef0ca8fcff0
Hazurl <ricoujules69@gmail.com>
2017-10-26 16:53:41 +0200

    is_mask_policy

--- include/ecs/policy/mask_policy.hpp
+++ include/ecs/policy/mask_policy.hpp
@@ -18,8 +18,27 @@ struct entity_mask {
     static constexpr i64 id_shift       = 0;
     static constexpr i64 version_shift  = id_bits_size;
 
-    static constexpr i64 id_mask        = ~((~0LL) << id_bits_size);
+    static constexpr i64 id_mask        = ~((~0LL) << id_bits_size     );
     static constexpr i64 version_mask   = ~((~0LL) << version_bits_size);
 };
+
+template<typename entity_mask_policy, typename = void>
+struct is_entity_mask_policy : mtp::boolConst<false> {};
+
+template<typename entity_mask_policy>
+struct is_entity_mask_policy<entity_mask_policy, mtp::Nothing<
+        std::enable_if_t<std::is_same<typename entity_mask_policy::tag, tag_mask_policy>::value>,
+        mtp::Constant<i64, entity_mask_policy::total_size>,
+        mtp::Constant<i64, entity_mask_policy::id_size>,
+        mtp::Constant<i64, entity_mask_policy::version_size>,
+        mtp::Constant<i64, entity_mask_policy::id_shift>,
+        mtp::Constant<i64, entity_mask_policy::version_shift>,
+        mtp::Constant<i64, entity_mask_policy::id_mask>,
+        mtp::Constant<i64, entity_mask_policy::version_mask>
+    >> : mtp::boolConst<true> {};
+
+using entity_32_mask = entity_mask<24, 8>;
+
+static_assert(is_entity_mask_policy<entity_32_mask>::value, "!");
 
 ECS_END_NS
