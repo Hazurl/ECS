9ea3bffd8006e67d9fc4d7a7cfa90282c4560640
Hazurl <ricoujules69@gmail.com>
2017-11-11 18:54:23 +0100

    update ecs root file

--- include/ecs/ecs.hpp
+++ include/ecs/ecs.hpp
@@ -1,7 +1,8 @@
 #include <ecs/component/ComponentPool.hpp>
 #include <ecs/controller/Controller.hpp>
-#include <ecs/controller/context/Context.hpp>
-#include <ecs/controller/context/Systems.hpp>
+#include <ecs/context/Context.hpp>
+#include <ecs/context/make/make_ctx.hpp>
+#include <ecs/context/Systems.hpp>
 #include <ecs/entity/Entity.hpp>
 #include <ecs/entity/EntityManager.hpp>
 #include <ecs/policy/grow_policy.hpp>
